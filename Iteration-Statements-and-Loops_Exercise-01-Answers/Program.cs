26055c6ef44dff70e92b0c3ad3f60a33ad9e7992
Stephen Michaels <stephen.michaels3333@gmail.com>
2023-03-15 13:43:38 -0400

    initial commit

--- Iteration-Statements-and-Loops_Exercise-01-Answers/Program.cs
+++ Iteration-Statements-and-Loops_Exercise-01-Answers/Program.cs
@@ -10,12 +10,20 @@ namespace Iteration_Statements_and_Loops_Exercise_01_Answers
 
             int[] arr1 = new int[] { 1, 7, 9, 4, 2, 3, 8, 10 };
             // TODO: Create a foreach loop to iterate through arr1 and print it's elements to the console
-
+            
+            foreach (var number in arr1)
+                {
+                    Console.WriteLine(number);   
+                }
 
             AddSpaces();
 
 
             // TODO: Create a for loop to iterate through arr1 IN REVERSE ORDER and print it's elements to the console
+            for (int i = arr1.Length - 1 ; i >= 0; i--) 
+            {
+                Console.WriteLine(arr1[i]);
+            }
 
 
             AddSpaces();
@@ -26,12 +34,28 @@ namespace Iteration_Statements_and_Loops_Exercise_01_Answers
             // TODO: Create a do-while loop that asks the user for a number and then adds it to a list called numbers
             // TODO: Make the do-while loop terminate when the user types 'exit' 
 
+            do
+            {
+                Console.WriteLine("Type 'quit' to exit the loop.");
+                Console.Write("Give me a number and I will add it to the list:");
+                userInput = Console.ReadLine().ToLower();
+                AddSpaces();
+                var canParse = int.TryParse(userInput, out int num);
+                numbers.Add(num);
+            } while (userInput != "quit");
+            
+
 
             AddSpaces();
 
 
             var index = 0;
             // TODO: Create a while loop that prints out all the nums inside numbers list to the console
+            while (numbers[index] != numbers[numbers.Count - 1])
+            {
+                Console.WriteLine(numbers[index]);
+                index ++;
+            }
 
 
 
